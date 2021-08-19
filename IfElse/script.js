4d22ff379da435663864180ff7b4b09118c03185
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-08-19 17:37:48 +0200

    If Else

---
+++ IfElse/script.js
@@ -0,0 +1,22 @@
+const Age = 18
+
+if (Age > 17) {
+    console.log ("je mag naar binnen")
+} else {
+    console.log ("je mag niet naar binnen")
+}
+const isFemale = Boolean
+
+if (Boolean = true) {
+    console.log ("je bent een vrouw")
+} else {
+    console.log ("je bent geen vrouw")
+}
+
+const driverStatus = Boolean
+
+if (Bob = false) {
+    console.log ("je bent Bob en je mag rijden")
+} else {
+    console.log ("je bent geen Bob en je mag niet rijden")
+}
