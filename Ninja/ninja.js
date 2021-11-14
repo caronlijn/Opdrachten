5234f4e04776c98c6d71def902bfa0e0f767ed3e
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-11-14 23:53:56 +0100

    Opdracht comparison en logical

---
+++ Ninja/ninja.js
@@ -0,0 +1,57 @@
+var myAge = 26;
+const firstName = "Bram" 
+const totalAmount = 58
+
+if (myAge >= 18 ){
+
+    document.write ("Je mag naar binnen");
+    
+}
+else {
+
+    document.write ("je mag niet naar binnen");
+
+}
+
+
+if (myAge >= 18 && firstName === "Bram" ){
+
+    document.write ("Je krijgt een gratis biertje");
+    
+}
+else {
+
+    document.write ("");
+
+}
+if (myAge >= 18 && myAge <= 25){
+
+    document.write (" You get 50% off!");
+   
+}
+else {
+    
+    document.write (" ")
+}
+
+if (totalAmount > 25){
+
+    document.write ("je krijgt bitterballen");
+}
+else {
+
+}
+if (totalAmount >= 50){
+
+    document.write ("je krijgt nachos");
+}
+else {
+
+}
+if (totalAmount >= 100){
+
+    document.write ("je krijgt champagne");
+}
+else {
+
+}
