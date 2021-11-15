c51fbeaf6205938b1898b09b0e87caf30012cfee
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-11-15 22:46:24 +0100

    Verven

---
+++ Verven/Pizza/script.js
@@ -0,0 +1,18 @@
+const muurVerven = function(wall, color){
+    console.log ("De" + wall +  "-muur is" + color + " geverfd!");
+}
+const color1 = " Rood";
+const color2 = " Groen";
+const color3 = " Blauw";
+const color4 = " Geel";
+const wall4 = " West";
+const wall1 = " Noord";
+const wall2 = " Oost";
+const wall3 = " Zuid";
+
+muurVerven (wall4,color4);
+muurVerven (wall1, color1);
+muurVerven (wall2,color2);
+muurVerven (wall3,color3);
+
+
