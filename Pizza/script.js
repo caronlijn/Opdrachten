e33f56486148de5116ef2deaf9181c176f17714d
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-11-15 21:48:33 +0100

    Pizza en Sushi

---
+++ Pizza/script.js
@@ -0,0 +1,20 @@
+const makePizza = function (){
+    console.log ("Maak deeg");
+    console.log ("Giet de sauce op het deeg");
+    console.log ("Beleg met lekkers");
+    console.log ("Doe de pizza in de oven");
+}
+
+
+const makeSushi = function (){
+    console.log ("Kook rijst");
+    console.log ("rol zwart blad uit");
+    console.log ("voeg rijst toe");
+    console.log ("voeg vis toe");
+    console.log ("rol alle op");
+}
+makeSushi ();
+makeSushi ();
+makeSushi ();
+makePizza ();
+makePizza ();
