7c5636b22869af3deaf32c8e10409c5343d8a421
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-11-18 23:46:48 +0100

    driemanieren

---
+++ drie_manieren/script.js
@@ -0,0 +1,34 @@
+//function declaration
+function squaredDouble (){
+    
+}
+    const a = 2;
+    const b = 4;
+    const c = a * a;
+    const d = b * b;
+    const e = c + d;
+    const f = e * e;
+    console.log ("squareDouble " + f);
+
+//function expression
+const squaredDouble2 = function() { }
+    const a2 = 1;
+    const b2 = 3;
+    const c2 = a2 * a2;
+    const d2 = b2 * b2;
+    const e2 = c2 + d2;
+    const f2 = e2 * e2;
+    console.log ("squaredDouble2 " + f2);
+
+//function Arrow
+const squareDouble3 = () => {}
+    const a3 = 3;
+    const b3 = 5;
+    const c3 = a3 * a3;
+    const d3 = b3 * b3;
+    const e3 = c3 + d3;
+    const f3 = e3 * e3;
+    console.log ("squaredDouble3 " + f3);  
+
+
+
