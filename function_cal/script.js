d88a29459d9a64333336b466bab476e91f5a102e
caronlijn <86471564+caronlijn@users.noreply.github.com>
2021-11-20 16:29:11 +0100

    cal_function

---
+++ function_cal/script.js
@@ -0,0 +1,36 @@
+const checkAge = function(age){
+    if (age > 18) {
+    return "Hello there";   
+    } 
+    else {
+    return "Hello Kiddo";
+    }
+}
+    console.log (checkAge(17));
+    console.log (checkAge(19));
+    console.log (checkAge(18));
+
+//Vat
+
+const bedragInclbtw = function (bedragExclbtw, Vat){
+    return bedragExclbtw + Vat;
+};
+const pc = bedragInclbtw (100, 21);
+console.log (pc);
+
+const checkVat = function (good, food, none){
+    if (good = 21) {
+        return "hoge btw " + bedragExclbtw + 21/100;}
+    
+    else {
+        return ("lage btw " + 9/21)
+    }
+}
+const good = pc;
+console.log)
+
+//console.log (bedragInclbtw(100, 21));
+
+//const Vat = function (good, food, none){
+
+//}
